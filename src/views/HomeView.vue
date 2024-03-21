b8a628aabcedaf857ba9c508378181f79276a172
Andres Campuzano Garzon <campuxano88@gmail.com>
2024-03-21 16:18:24 -0500

    chore: cleaning project from demo files/styles/code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1>HomeView</h1>
   </main>
 </template>
+
+<script setup lang="ts">
+</script>
