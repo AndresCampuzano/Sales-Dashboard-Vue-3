c279f334d9bc972bc2721426352895f08ab21156
Andres Campuzano Garzon <campuxano88@gmail.com>
2024-03-21 20:29:42 -0500

    feature: Add new Button and Input components and refactor HomeView.vue

---
+++ src/components/inputs/Input.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="mb-6">
+    <label v-if="label" :for="props.id" class="block mb-2 text-sm font-medium">{{ label }}</label>
+    <input :value="modelValue" @input="$emit('update:modelValue', $event?.target?.value)" :type="type" :id="id" class="border text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400" :placeholder="placeholder" :required="required" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { defineProps, defineEmits } from "vue"
+
+const props = defineProps({
+  modelValue: {
+    required: true,
+    type: String
+  },
+  id: {
+    required: true,
+    type: String
+  },
+  type: {
+    type: String,
+    default: 'text'
+  },
+  placeholder: {
+    type: String,
+    default: ""
+  },
+  label: {
+    type: String,
+    default: ""
+  },
+  required: {
+    type: Boolean,
+    default: false
+  },
+})
+defineEmits(['update:modelValue'])
+
+
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,23 @@
 <template>
   <main>
-    <form class="m-6">
-      <div class="mb-6">
-        <label for="email" class="block mb-2 text-sm font-medium">Correo</label>
-        <input type="email" id="email" class="border text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400" placeholder="correo@shop.com" required />
-      </div>
-      <div class="mb-6">
-        <label for="password" class="block mb-2 text-sm font-medium">Contraseña</label>
-        <input type="password" id="password" class="border text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400" placeholder="•••••••••" required />
-      </div>
-      <button type="submit" class="bg-slate-800 hover:bg-blue-900 focus:ring-blue-500 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center ">Submit</button>
+    <form class="m-6" @submit.prevent="onSubmit">
+      <Input v-model="email" id="email" type="email" placeholder="correo@shop.com" label="Correo" required/>
+      <Input v-model="password" id="password" type="password" placeholder="•••••••••" label="Contraseña" required/>
+      <button type="submit" class="bg-slate-800 hover:bg-blue-900 focus:ring-blue-500 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</button>
     </form>
   </main>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+import Input from '@/components/inputs/Input.vue'
+
+const email = ref('');
+const password = ref('');
+
+function onSubmit(): void {
+  console.log(email.value)
+  console.log(password.value)
+}
+
 </script>
