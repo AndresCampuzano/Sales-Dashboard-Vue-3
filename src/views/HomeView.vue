bd45270eecf76ec12871eb269128f6c094dc0868
Andres Campuzano Garzon <campuxano88@gmail.com>
2024-03-23 20:23:18 -0500

    wip: SummaryItem.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,8 +4,28 @@
     <p>user: {{ user?.displayName }}</p>
     <FormButton text="Cerrar sesión" @click="onLogOut" style-type="secondary" />
     <section class="max-w-xl m-auto">
-      <p v-if="state.loading">Cargando...</p>
-      <summary-item v-for="(item, _) in state.data" :key="item.id" :data="item" />
+      <template v-if="state.loading">
+        <div
+          v-for="(_, index) in 15"
+          class="m-3 p-2.5 bg-gray-800 rounded-md cursor-pointer"
+          :key="index"
+        >
+          <div class="animate-pulse flex space-x-4">
+            <div class="rounded-full bg-slate-700 h-16 w-16"></div>
+            <div class="flex-1 space-y-6 py-2">
+              <div class="h-2 bg-slate-700 rounded"></div>
+              <div class="space-y-3">
+                <div class="grid grid-cols-3 gap-4">
+                  <div class="h-2 bg-slate-700 rounded col-span-2"></div>
+                  <div class="h-2 bg-slate-700 rounded col-span-1"></div>
+                </div>
+                <div class="h-2 bg-slate-700 rounded"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </template>
+      <summary-item v-for="item in state.data" :key="item.id" :data="item" />
     </section>
   </main>
 </template>
