232fd06efd6e3f8785d054d8fa0aed69d97b1951
AndresCampuzano <campuxano88@gmail.com>
2024-04-16 22:16:05 -0500

    change bg style on modal

--- src/components/CustomModal.vue
+++ src/components/CustomModal.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-    <div class="fixed inset-0 backdrop-blur-sm bg-opacity-75 transition-opacity"></div>
+    <div class="bg-gray-900 fixed inset-0 backdrop-blur-md bg-opacity-70 transition-opacity"></div>
     <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
       <div
         class="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0"
       >
         <div
-          class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg w-full max-w-lg"
+          class="relative transform overflow-hidden rounded-lg text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg w-full max-w-lg"
         >
-          <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+          <div class="bg-gray-800 px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
             <div class="sm:flex sm:items-start">
               <div
                 class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full sm:mx-0 sm:h-10 sm:w-10"
@@ -31,18 +31,18 @@
                 </svg>
               </div>
               <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                <h3 class="text-base font-semibold leading-6 text-gray-900" id="modal-title">
+                <h3 class="text-base font-semibold leading-6" id="modal-title">
                   {{ title }}
                 </h3>
                 <div class="mt-2">
-                  <p class="text-base text-gray-500">
+                  <p class="text-base">
                     {{ description }}
                   </p>
                 </div>
               </div>
             </div>
           </div>
-          <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+          <div class="bg-gray-800 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
             <FormButton
               :text="primaryButton"
               @click="onSubmit"
